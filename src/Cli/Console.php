98429329899b81e074067a9df3d2b54ddb358cf5
Alexey Berezkin <C:\Users\Alex\AppData\Roaming\The Bat!>
2014-11-24 11:02:41 +0200

    Fix typo

--- src/Cli/Console.php
+++ src/Cli/Console.php
@@ -92,7 +92,7 @@ class Console
     {
         set_error_handler(
             function ($errno, $errstr, $errfile, $errline ) {
-                throw new ErrorException($errstr, $errno, 0, $errfile, $errline);
+                throw new \ErrorException($errstr, $errno, 0, $errfile, $errline);
             },
             error_reporting()
         );
